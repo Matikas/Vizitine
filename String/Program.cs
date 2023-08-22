b81f997f913eea30ff3ef87f3100fb6b0997ad39
Marius Paškevičius <marius.paskevicius@yahoo.com>
2023-08-22 18:56:59 +0300

    removed unused code

--- String/Program.cs
+++ String/Program.cs
@@ -1,10 +1,7 @@
 ﻿int qty = 5;
 int price = 2;
 string product = "obuolys";
-string tel = "+3706123456";
 
-Console.WriteLine("================================");
-Console.WriteLine();
 Console.WriteLine("Preke:" + product);
 Console.WriteLine("kiekis: " + qty);
 Console.WriteLine("kaina: " + price + " eur");
