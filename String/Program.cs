e5691390dbf02e052c366d999a890316560353a3
Marius Paškevičius <marius.paskevicius@yahoo.com>
2023-08-22 18:49:31 +0300

    Add project files.

---
+++ String/Program.cs
@@ -0,0 +1,11 @@
+﻿int qty = 5;
+int price = 2;
+string product = "obuolys";
+string tel = "+3706123456";
+
+Console.WriteLine("================================");
+Console.WriteLine();
+Console.WriteLine("Preke:" + product);
+Console.WriteLine("kiekis: " + qty);
+Console.WriteLine("kaina: " + price + " eur");
+Console.WriteLine("\n");
